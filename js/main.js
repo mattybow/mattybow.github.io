348ba14284fa46362c72100d4736f6987f98b46a
Matt Bow <mattybow@gmail.com>
2014-12-16 10:13:42 -1000

    particle js and first pane redesign

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ require.config({
 		imgloaded:"imagesloaded/imagesloaded",
 		skrollr:"skrollr/dist/skrollr.min",
 		fishbone:"fishbone.js/fishbone",
-		BlurStack:"../js/blurstack"
+		BlurStack:"../js/blurstack",
+		particlesJS:"particles.js/particles.min"
 	}
 });
 
@@ -14,7 +15,8 @@ require(["jquery",
 		"imgloaded",
 		"skrollr",
 		"fishbone",
-		"BlurStack"],
+		"BlurStack",
+		"particlesJS"],
 	function($,
 			imagesloaded,
 			skrollr,
@@ -43,6 +45,53 @@ require(["jquery",
 				drawBlur();
 			};
 		}
+
+		particlesJS('particles-js', {
+			particles: {
+				color: '#eee',
+				shape: 'circle', // "circle", "edge" or "triangle"
+				opacity: 0.5,
+				size: 2,
+				size_random: true,
+				nb: 150,
+				line_linked: {
+					enable_auto: true,
+					distance: 100,
+					color: '#eee',
+					opacity: 0.7,
+					width: 1,
+					condensed_mode: {
+						enable: false,
+						rotateX: 600,
+						rotateY: 600
+					}
+				},
+				anim: {
+					enable: true,
+					speed: 2
+				}
+			},
+			interactivity: {
+				enable: true,
+				mouse: {
+					distance: 250
+				},
+				detect_on: 'canvas', // "canvas" or "window"
+				mode: 'grab',
+				line_linked: {
+					opacity: .3
+				},
+				events: {
+					onclick: {
+						enable: true,
+						mode: 'push', // "push" or "remove" (particles)
+						nb: 4
+					}
+				}
+			},
+			/* Retina Display Support */
+			retina_detect: true
+		});
 	
 		/*window.App={};
 
